7ef5117ef6a7d3d00e32a9933dc93b225814bd2b
Ruravi <joseruravi@hotmail.com>
2022-10-25 18:40:45 -0500

    Update UserUpdateModel.cs

--- DreamCleaning/Core/Domain/Models/UserUpdateModel.cs
+++ DreamCleaning/Core/Domain/Models/UserUpdateModel.cs
@@ -4,9 +4,9 @@ namespace DC.WebApi.Core.Domain.Models
 {
     public class UserUpdateModel
     {
-        [StringLength(120), Required]
+        [StringLength(120)]
         public String Username { get; set; }
-        [StringLength(120, MinimumLength = 12), Required]
+        [StringLength(120, MinimumLength = 5)]
         public string Password { get; set; }
 
         public UserUpdateModel()
